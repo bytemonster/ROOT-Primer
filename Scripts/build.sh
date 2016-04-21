8fe5b15759bb45bd1b661624fe89406b08003462
bytrmonster <nefeli.kousi@gmail.com>
2016-04-21 14:08:16 +0200

    issue with kernel

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,6 @@ html="html"
 
 if [ "$1" = "pdf" ]
 then
-	cd ../Scripts
 	python removeJS.py ../notebooks/1-Motivation-and-Introduction.ipynb ../notebooks/1-Motivation-and-Introduction_NOJS.ipynb
 	python removeJS.py ../notebooks/2-ROOT-Basics.ipynb ../notebooks/2-ROOT-Basics_NOJS.ipynb 
 	python removeJS.py ../notebooks/3-ROOT-Macros.ipynb ../notebooks/3-ROOT-Macros_NOJS.ipynb 
@@ -14,20 +13,22 @@ then
 	python removeJS.py ../notebooks/7-File-IO-and-Parallel-Analysis.ipynb ../notebooks/7-File-IO-and-Parallel-Analysis_NOJS.ipynb 
 	python removeJS.py ../notebooks/8-ROOT-in-Python.ipynb ../notebooks/8-ROOT-in-Python_NOJS.ipynb 
 	python removeJS.py ../notebooks/9-Concluding-Remarks.ipynb ../notebooks/9-Concluding-Remarks_NOJS.ipynb
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='python3' --to notebook --execute ../notebooks/1-Motivation-and-Introduction_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/2-ROOT-Basics_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='ROOT' --to notebook --execute ../notebooks/3-ROOT-Macros_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='ROOT' --to notebook --execute ../notebooks/4-Graphs_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='ROOT' --to notebook --execute ../notebooks/5-Histograms_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='ROOT' --to notebook --execute ../notebooks/6-Functions-and-Parameter-Estimation_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='ROOT' --to notebook --execute ../notebooks/7-File-IO-and-Parallel-Analysis_NOJS.ipynb 
-	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='python3' --to notebook --execute ../notebooks/9-Concluding-Remarks_NOJS.ipynb
+	jupyter nbconvert --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/sqrt.ipynb
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='python3' --to notebook --execute ../notebooks/1-Motivation-and-Introduction_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/2-ROOT-Basics_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/3-ROOT-Macros_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/4-Graphs_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/5-Histograms_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/6-Functions-and-Parameter-Estimation_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='root' --to notebook --execute ../notebooks/7-File-IO-and-Parallel-Analysis_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='python2' --to notebook --execute ../notebooks/8-ROOT-in-Python_NOJS.ipynb 
+	jupyter nbconvert --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.kernel_name='python3' --to notebook --execute ../notebooks/9-Concluding-Remarks_NOJS.ipynb
 
 	# cd ../
-	# jupyter nbconvert --to pdf notebooks/1-Motivation-and-Introduction_NOJS.ipynb notebooks/2-ROOT-Basics_NOJS.ipynb notebooks/3-ROOT-Macros_NOJS.ipynb notebooks/4-Graphs_NOJS.ipynb notebooks/5-Histograms_NOJS.ipynb notebooks/6-Functions-and-Parameter-Estimation_NOJS.ipynb notebooks/7-File-IO-and-Parallel-Analysis_NOJS.ipynb notebooks/8-ROOT-in-Python_NOJS.ipynb notebooks/9-Concluding-Remarks_NOJS.ipynb
+	# jupyter nbconvert --to pdf notebooks/1-Motivation-and-Introduction_NOJS.nbconvert.ipynb notebooks/2-ROOT-Basics_NOJS.nbconvert.ipynb notebooks/3-ROOT-Macros_NOJS.nbconvert.ipynb notebooks/4-Graphs_NOJS.nbconvert.ipynb notebooks/5-Histograms_NOJS.nbconvert.ipynb notebooks/6-Functions-and-Parameter-Estimation_NOJS.nbconvert.ipynb notebooks/7-File-IO-and-Parallel-Analysis_NOJS.nbconvert.ipynb notebooks/8-ROOT-in-Python_NOJS.nbconvert.ipynb notebooks/9-Concluding-Remarks_NOJS.nbconvert.ipynb
 	cd ../MARKDOWN
-	jupyter nbconvert --to markdown ../notebooks/1-Motivation-and-Introduction_NOJS.ipynb ../notebooks/2-ROOT-Basics_NOJS.ipynb ../notebooks/3-ROOT-Macros_NOJS.ipynb ../notebooks/4-Graphs_NOJS.ipynb ../notebooks/5-Histograms_NOJS.ipynb ../notebooks/6-Functions-and-Parameter-Estimation_NOJS.ipynb ../notebooks/7-File-IO-and-Parallel-Analysis_NOJS.ipynb ../notebooks/8-ROOT-in-Python_NOJS.ipynb ../notebooks/9-Concluding-Remarks_NOJS.ipynb
-	pandoc --toc 1-Motivation-and-Introduction_NOJS.md 2-ROOT-Basics_NOJS.md 3-ROOT-Macros_NOJS.md 4-Graphs_NOJS.md 5-Histograms_NOJS.md 6-Functions-and-Parameter-Estimation_NOJS.md 7-File-IO-and-Parallel-Analysis_NOJS.md 8-ROOT-in-Python_NOJS.md 9-Concluding-Remarks_NOJS.md --latex-engine=xelatex -o ../ROOT-Primer.pdf
+	jupyter nbconvert --to markdown ../notebooks/1-Motivation-and-Introduction_NOJS.nbconvert.ipynb ../notebooks/2-ROOT-Basics_NOJS.nbconvert.ipynb ../notebooks/3-ROOT-Macros_NOJS.nbconvert.nbconvert.ipynb ../notebooks/4-Graphs_NOJS.nbconvert.ipynb ../notebooks/5-Histograms_NOJS.nbconvert.ipynb ../notebooks/6-Functions-and-Parameter-Estimation_NOJS.nbconvert.ipynb ../notebooks/7-File-IO-and-Parallel-Analysis_NOJS.nbconvert.ipynb ../notebooks/8-ROOT-in-Python_NOJS.nbconvert.ipynb ../notebooks/9-Concluding-Remarks_NOJS.nbconvert.ipynb
+	pandoc --toc 1-Motivation-and-Introduction_NOJS.nbconvert.md 2-ROOT-Basics_NOJS.nbconvert.md 3-ROOT-Macros_NOJS.nbconvert.md 4-Graphs_NOJS.nbconvert.md 5-Histograms_NOJS.nbconvert.md 6-Functions-and-Parameter-Estimation_NOJS.nbconvert.md 7-File-IO-and-Parallel-Analysis_NOJS.nbconvert.md 8-ROOT-in-Python_NOJS.nbconvert.md 9-Concluding-Remarks_NOJS.nbconvert.md --latex-engine=xelatex -o ../ROOT-Primer.pdf
 elif [ "$1" = "html" ]
 then
 	mkdir -p ../HTML
