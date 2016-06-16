6150c16a4c5f6fff477f01f58449b91daacf9511
bytrmonster <nefeli.kousi@gmail.com>
2016-06-16 14:27:02 +0200

    cleanup

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -11,6 +11,20 @@ NBEXTMD=.nbconvert.md
 NBLISTPDF=`echo 1-Motivation-and-Introduction 2-ROOT-Basics 3-ROOT-Macros 4-Graphs 5-Histograms 6-Functions-and-Parameter-Estimation 7-File-IO-and-Parallel-Analysis 8-ROOT-in-Python 9-Concluding-Remarks`
 NBLISTHTML=`echo 0_ROOT_Primer_TOC 1-Motivation-and-Introduction 2-ROOT-Basics 3-ROOT-Macros 4-Graphs 5-Histograms 6-Functions-and-Parameter-Estimation 7-File-IO-and-Parallel-Analysis 8-ROOT-in-Python 9-Concluding-Remarks`
 
+
+if [ "$1" = "html" ] || [ "$1" = "all" ]
+then
+	chapter=0
+	for name in $NBLISTHTML ;do
+	 	# python addtitle.py $NBDIR/$name"$NBEXT"
+		jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html $NBDIR/$name"$NBEXT"
+		python html-post-prossesor_BS.py $NBDIR/$name"$HTML" $NBDIR/$name.prep"$HTML" $chapter
+		python posthtml.py $NBDIR/$name.prep"$HTML" 
+		let chapter=chapter+1
+	done
+	python makehtml.py $NBDIR/*.prep.html
+	rm $NBDIR/*.html
+fi
 if [ "$1" = "pdf" ] || [ "$1" = "all" ]
 then
 	for name in $NBLISTPDF ;do
@@ -23,19 +37,6 @@ then
 	pdftk frontpage.pdf ../ROOT-Primer_v0.pdf output ../ROOT-Primer.pdf 
 	rm -rf *NOJS*
 	rm ../ROOT-Primer_v0.pdf
-fi
-if [ "$1" = "html" ] || [ "$1" = "all" ]
-then
-	chapter=0
-	for name in $NBLISTHTML ;do
-	 	python addtitle.py $NBDIR/$name"$NBEXT"
-		jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html $NBDIR/$name"$NBEXT"
-		python html-post-prossesor_BS.py $NBDIR/$name"$HTML" $NBDIR/$name.prep"$HTML" $chapter
-		python posthtml.py $NBDIR/$name.prep"$HTML" 
-		let chapter=chapter+1
-	done
-	python makehtml.py $NBDIR/*.prep.html
-	rm $NBDIR/*.html
 fi
 if [ "$1" != "html" ] && [ "$1" != "all" ] && [ "$1" != "pdf" ] 
 then
--- Scripts/mdconfig.py
+++
@@ -1,2 +0,0 @@
-c = get_config()
-c.FilesWriter.build_directory = "../MARKDOWN"
