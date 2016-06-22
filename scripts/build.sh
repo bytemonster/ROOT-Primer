54d28ab101e246c6d55bfcf51eb32a181e34200c
bytrmonster <nefeli.kousi@gmail.com>
2016-06-22 11:40:07 +0200

    html works from other folders. PDF onlt from scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ get_abs_filename() {
   echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-BASEDIR=$(cd $(dirname $0);pwd)
+BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
 NBDIR=$BASEDIR/../notebooks
 NBEXT=.ipynb
 HTML=.html
@@ -23,23 +23,24 @@ then
 	chapter=0
 	for name in $NBLISTHTML ;do
 		jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html $NBDIR/$name"$NBEXT"
-		python html-post-processor_BS.py $name"$HTML" $name.prep"$HTML" $chapter
-		python posthtml.py $NBDIR/$name.prep"$HTML" 
+		python $BASEDIR/html-post-processor_BS.py $name"$HTML" $name.prep"$HTML" $chapter
+		python $BASEDIR/posthtml.py $NBDIR/$name.prep"$HTML" 
 		let chapter=chapter+1
 	done
-	python makehtml.py $NBDIR/*.prep.html
+	python $BASEDIR/makehtml.py $NBDIR/*.prep.html
 	find $NBDIR -type f -name "*.html" -and -not -name "final.html" | xargs rm
 	# rm $NBDIR/*.html
 fi
 if [ "$1" = "pdf" ] || [ "$1" = "all" ]
 then
 	for name in $NBLISTPDF ;do
-	 	python removeJS.py $NBDIR/$name$NBEXT $NBDIR/"$name"_NOJS"$NBEXT";
+	 	python $BASEDIR/removeJS.py $NBDIR/$name$NBEXT $NBDIR/"$name"_NOJS"$NBEXT";
 		jupyter nbconvert --ExecutePreprocessor.timeout=2000 --to notebook --execute $NBDIR/"$name"_NOJS"$NBEXT";
 		jupyter nbconvert --to markdown $NBDIR/"$name"_NOJS"$NBEXTNEW" --template=mytemplate.tpl;
 	done
 	cd $NBDIR
-	pandoc --toc --template=../scripts/mdtemplate.tex $NBDIR/*_NOJS$NBEXTMD --latex-engine=pdflatex -o $NBDIR/ROOT-Primer_v0.pdf
+	echo $BASEDIR
+	pandoc --toc --template=$BASEDIR/mdtemplate.tex $NBDIR/*_NOJS$NBEXTMD --latex-engine=pdflatex -o $NBDIR/ROOT-Primer_v0.pdf
 	pdftk frontpage.pdf $NBDIR/ROOT-Primer_v0.pdf output $NBDIR/ROOT-Primer.pdf
 	rm -rf *NOJS*
 	rm $NBDIR/ROOT-Primer_v0.pdf
